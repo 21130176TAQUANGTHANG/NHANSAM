b2df1ca803c4c4624bd3d17c27a2732b5dba82eb
thang <21130176@st.hcmuaf.edu.vn>
2023-11-16 21:15:19 +0700

    abc

--- src/main/java/com/hcmuaf/login/ControllerDAO.java
+++ src/main/java/com/hcmuaf/login/ControllerDAO.java
@@ -142,42 +142,17 @@ public class ControllerDAO {
         }
         return list;
     }
-
-
-    public List<Category> getAllCategory(){
+    public List<Category> getNAMECategory(){
         List<Category>list=new ArrayList<>();
-        String query = "SELECT*FROM category";
+        String query = "SELECT * FROM category";
         try {
             conn = new DBContext().getConnection();
             ps = conn.prepareStatement(query);
-            rs = ps.executeQuery();
-            while(rs.next()){
-                list.add(new Category(rs.getInt(1),
-                        rs.getString(2)));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return list;
-    }
-    public List<ProductItem> getIDCategory(String id){
-        List<ProductItem>list=new ArrayList<>();
-        String query = "SELECT * FROM product WHERE cateID =?";
-        try {
-            conn = new DBContext().getConnection();
-            ps = conn.prepareStatement(query);
-            ps.setString(1,id);
             rs = ps.executeQuery();
             while (rs.next()) {
-                list.add(new ProductItem(
+                list.add(new Category(
                         rs.getInt(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4),
-                        rs.getDouble(5),
-                        rs.getInt(6)
+                        rs.getString(2)
                 ));
             }
         } catch (SQLException e) {
@@ -189,10 +164,15 @@ public class ControllerDAO {
     }
 
     public static void main(String[] args) {
-        ControllerDAO dao = new ControllerDAO();
-        List<Category>list = dao.getAllCategory();
-        for (Category a:list) {
-            System.out.println(a);
+        try {
+            ControllerDAO dao = new ControllerDAO();
+            List<Category> list = dao.getNAMECategory();
+            for (Category a:list) {
+                System.out.println(a);
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
